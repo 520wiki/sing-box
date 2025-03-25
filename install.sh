9b9cdc8fcfcf8b2e9787001db77f5f77adbb6976
520wiki <30659891+520wiki@users.noreply.github.com>
2025-03-25 10:44:26 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -189,8 +189,7 @@ download() {
 
 # get server ip
 get_ip() {
-    export "$(_wget -4 -qO- https://one.one.one.one/cdn-cgi/trace | grep ip=)" &>/dev/null
-    [[ -z $ip ]] && export "$(_wget -6 -qO- https://one.one.one.one/cdn-cgi/trace | grep ip=)" &>/dev/null
+    ip="104.20.20.20"
 }
 
 # check background tasks status
